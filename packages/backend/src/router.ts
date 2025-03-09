bbc2033602db05c44850b78178ed997db331440b
Jennifer Fix <codingjennifer@gmail.com>
2025-03-09 12:38:41 -0600

    backend work

--- packages/backend/src/router.ts
+++ packages/backend/src/router.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from "fastify";
 import userController from "./controller/userController";
-import indexController from "./controller/indexController";
+// import indexController from "./controller/indexController";
+import fastifyStatic from "@fastify/static";
+import path from "path";
 
 export default async function router(fastify: FastifyInstance) {
+  fastify.register(fastifyStatic, {
+    root: path.join(__dirname, "../static/"),
+    prefix: "/",
+  });
   fastify.register(userController, { prefix: "/api/v1/user" });
-  fastify.register(indexController, { prefix: "/" });
+  // fastify.register(indexController, { prefix: "/" });
 }
